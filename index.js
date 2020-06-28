b05d4ca9a729c0755ac7b77a913c6e2ea40c6246
Pewww <jung22337@naver.com>
2020-06-28 14:34:55 +0900

    :construction: query로 데이터 불러오기 성공

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi');
+const {GraphQLServer} = require('graphql-yoga');
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, {name}) => `Hello ${name || 'World'}!`
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('Server is running on 4000 port.');
+});
